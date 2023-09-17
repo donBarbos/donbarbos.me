8ef8295c6d77bb1b33104d556597dbcb6b3207ff
donBarbos <donbarbos@proton.me>
2023-09-17 17:09:51 +0300

    correct paths

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -20,9 +20,9 @@ export default async function ProjectsPage() {
 		return acc;
 	}, {} as Record<string, number>);
 
-	const featured = allProjects.find((project) => project.slug === "unkey")!;
-	const top2 = allProjects.find((project) => project.slug === "planetfall")!;
-	const top3 = allProjects.find((project) => project.slug === "highstorm")!;
+  const featured = allProjects.find((project) => project.slug === "telegram-bot-template")!;
+  const top2 = allProjects.find((project) => project.slug === "md-share")!;
+  const top3 = allProjects.find((project) => project.slug === "yale-celi-list-parser")!;
 	const sorted = allProjects
 		.filter((p) => p.published)
 		.filter(
